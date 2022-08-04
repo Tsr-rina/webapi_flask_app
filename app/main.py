35c83707938ef090a855cafd7f013d4da86b4a82
rinas <topi2frie3elec@gmail.com>
2022-08-04 20:58:30 +0900

    add ajax

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, url_for
+# from flask import Flask, render_template, url_for
+from flask import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello():
     return render_template("index.html")
+
+@app.route("/test_ajax/", methods=["GET", "POST"])
+def test_ajax():
+    value = request.args.get("value")
+    value = str(value)
+    result = value + ":日本です"
+    return jsonify({"result":result})
 
 
 if __name__ == "__main__":
