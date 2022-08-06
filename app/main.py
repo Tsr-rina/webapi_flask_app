befd31dcf6bd70cd20e424b17e20785b0286babb
rinas <topi2frie3elec@gmail.com>
2022-08-06 21:10:57 +0900

    soso OK

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ def index():
     p_name = []
     p_kana = []
     p_exp = []
+    a_country = []
 
     res = requests.get(url)
     data_all = json.loads(res.text)
@@ -27,8 +28,8 @@ def index():
         p_name.append(data_all[country[cn]][0]['product_'+str(country[cn][:1])+str(pn)][0]['p_name'])
         p_kana.append(data_all[country[cn]][0]['product_'+str(country[cn][:1])+str(pn)][0]['p_kana'])
         p_exp.append(data_all[country[cn]][0]['product_'+str(country[cn][:1])+str(pn)][0]['p_explanation'])
-
-    return render_template("index.html", name=p_name, kana=p_kana, explain=p_exp)
+        a_country.append(data_all[country[cn]][0]['product_'+str(country[cn][:1])+str(pn)][0]['author'][0]['a_from'])
+    return render_template("index.html", name=p_name, kana=p_kana, explain=p_exp, country=a_country)
 
 @app.route("/test_ajax/", methods=["GET", "POST"])
 def test_ajax():
@@ -40,6 +41,7 @@ def test_ajax():
     a_kana = []
     a_birth = []
     a_exp = []
+    a_country = []
     c = request.args.get("value")
     if c == "all":
         res = requests.get(url)
@@ -59,8 +61,9 @@ def test_ajax():
                 a_kana.append(data_all[country[i]][0]['product_'+str(c_m[:1])+str(j)][0]['author'][0]['a_kana'])
                 a_birth.append(data_all[country[i]][0]['product_'+str(c_m[:1])+str(j)][0]['author'][0]['a_birthday'])
                 a_exp.append(data_all[country[i]][0]['product_'+str(c_m[:1])+str(j)][0]['author'][0]['a_info'])
+                a_country.append(data_all[country[i]][0]['product_'+str(c_m[:1])+str(j)][0]['author'][0]['a_from'])
 
-        return jsonify({"product":[p_name, p_kana, p_exp], "author":[a_name, a_kana, a_birth, a_exp]})
+        return jsonify({"product":[p_name, p_kana, p_exp], "author":[a_name, a_kana, a_birth, a_exp, a_country]})
         
     else:
         res = requests.get(url+c)
@@ -73,12 +76,39 @@ def test_ajax():
             a_kana.append(data[0]['product_'+str(c[:1])+str(i)][0]['author'][0]['a_kana'])
             a_birth.append(data[0]['product_'+str(c[:1])+str(i)][0]['author'][0]['a_birthday'])
             a_exp.append(data[0]['product_'+str(c[:1])+str(i)][0]['author'][0]['a_info'])
-        return jsonify({"product":[p_name, p_kana, p_exp], "author":[a_name, a_kana, a_birth, a_exp]})
+            a_country.append(data_all[country[i]][0]['product_'+str(c_m[:1])+str(j)][0]['author'][0]['a_from'])
+
+        return jsonify({"product":[p_name, p_kana, p_exp], "author":[a_name, a_kana, a_birth, a_exp ,a_country]})
     # return jsonify({"result":url+c})
 
+@app.route("/popup_ajax/", methods=["GET", "POST"])
+def popup():
+    a_name = ""
+    a_kana = ""
+    a_birth = ""
+    a_exp = ""
+
+    name = request.args.get("name")
+    country = request.args.get("country")
+    
+    res = requests.get(url + country)
+    data = json.loads(res.text)
+
+    for i in range(1, len(data[0])+1):
+        tmp = data[0]['product_'+str(country[:1])+str(i)][0]['p_name']
+
+        if tmp == name:
+            a_name = data[0]['product_'+str(country[:1])+str(i)][0]['author'][0]['a_name']
+            a_kana = data[0]['product_'+str(country[:1])+str(i)][0]['author'][0]['a_kana']
+            a_birth = data[0]['product_'+str(country[:1])+str(i)][0]['author'][0]['a_birthday']
+            a_exp = data[0]['product_'+str(country[:1])+str(i)][0]['author'][0]['a_info']
+
+    return jsonify({"author":[a_name, a_kana, a_birth, a_exp]})
+
+
 def randoms(s, e):
-  n = random.randint(s, e)
-  return n
+    n = random.randint(s, e)
+    return n
 
 
 if __name__ == "__main__":
